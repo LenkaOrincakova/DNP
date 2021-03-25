eb14e147286c9274c742bc8e5b6d870e4622bced
littlelenka3 <56080011+littlelenka3@users.noreply.github.com>
2021-03-25 14:09:01 +0100

    Add files via upload

---
+++ Session3/E01/E01/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E01
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public double Height { get; set; }
+        public bool IsMarried { get; set; }
+        public char Sex { get; set; }
+        public string [] Hobbies { get; set; }
+
+
+    }
+}
---
+++ Session3/E01/E01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace E01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Lenka = new Person
+            {
+                FirstName = "Lenka",
+                LastName = "Orincakova",
+                Age = 20,
+                Height = 159,
+                IsMarried = false,
+                Sex = 'f',
+                Hobbies = new string[] { "gym, studying" }
+            };
+            var Jano = new Person
+            {
+                FirstName = "jano",
+                LastName = "lishak",
+                Age = 20,
+                Height = 179,
+                IsMarried = false,
+                Sex = 'm',
+                Hobbies = new string[] { "programming, dron" }
+            };
+
+            var Whatever = new Person
+            {
+                FirstName = "llell",
+                LastName = "lllll",
+                Age = 34,
+                Height = 390,
+                IsMarried = false,
+                Sex = 'm',
+                Hobbies = new string[] { "njn, kmik" }
+            };
+
+            var people = new List<Person> { Lenka, Jano, Whatever };
+            var peopleJson = JsonSerializer.Serialize(people, new JsonSerializerOptions { WriteIndented = true });
+            Console.WriteLine(peopleJson);
+        }
+    }
+}
