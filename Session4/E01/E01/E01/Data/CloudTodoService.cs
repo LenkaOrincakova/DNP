510893fd24f46c807270add2071e7367636d66cb
littlelenka3 <56080011+littlelenka3@users.noreply.github.com>
2021-03-25 14:12:36 +0100

    Add files via upload

---
+++ Session4/E01/E01/E01/Data/CloudTodoService.cs
@@ -0,0 +1,52 @@
+﻿using E01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace E01.Data
+{
+    public class CloudTodoService : ITodoService
+    {
+        private readonly HttpClient client;
+        private string uri = "http://localhost:5003";
+            
+
+        public CloudTodoService()
+        {
+          client = new HttpClient();
+
+        }
+        public async Task<IList<Todo>> GetTodosAsync()
+        {
+            Task<string> stringAsync = client.GetStringAsync(uri + "/todos");
+            string message = await stringAsync;
+            List<Todo> result = JsonSerializer.Deserialize<List<Todo>>(message);
+            return result;
+        }
+        public async Task AddTodoAsync(Todo todo)
+        {
+            string todoAsJson = JsonSerializer.Serialize(todo);
+            HttpContent content = new StringContent(todoAsJson, Encoding.UTF8, "application/json");
+            await client.PostAsync(uri + "/todos", content);
+        }
+
+       
+
+        public async Task RemoveTodoAsync(int todoId)
+        {
+            await client.DeleteAsync($"{uri}/todos/{todoId}");
+        }
+
+        public async Task UpdateAsync(Todo todo)
+        {
+            string todoAsJson = JsonSerializer.Serialize(todo);
+            HttpContent content = new StringContent(todoAsJson, Encoding.UTF8, "application/json");
+            await client.PatchAsync($"{uri}/todos/{todo.TodoId}", content);
+        }
+    }
+}
---
+++ Session4/E01/E01/E01/Data/ITodoService.cs
@@ -0,0 +1,16 @@
+﻿using E01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E01.Data
+{
+    interface ITodoService
+    {
+       Task<IList<Todo>> GetTodosAsync();
+        Task AddTodoAsync(Todo todo);
+        Task RemoveTodoAsync(int todoId);
+        Task UpdateAsync(Todo todo);
+    }
+}
