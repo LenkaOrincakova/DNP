640ff28299e0fa9d8202247a3e1528aca83230b3
littlelenka3 <56080011+littlelenka3@users.noreply.github.com>
2021-03-25 14:10:37 +0100

    Add files via upload

---
+++ Session3/E03/E03/Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace E03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting client..");
+
+            TcpClient client = new TcpClient("127.0.0.1", 5000);
+
+            NetworkStream stream = client.GetStream();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                byte[] dataToServer = Encoding.ASCII.GetBytes("Hello from client");
+                stream.Write(dataToServer, 0, dataToServer.Length);
+
+                byte[] dataFromServer = new byte[1024];
+                int bytesRead = stream.Read(dataFromServer, 0, dataFromServer.Length);
+                string respone = Encoding.ASCII.GetString(dataFromServer, 0, bytesRead);
+                Console.WriteLine(respone);
+                if(input == "exit")
+                {
+                    break;
+                }
+            }
+
+            stream.Close();
+            client.Close();
+
+        }
+    }
+}
---
+++ Session3/E04/E04/E04/Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace E04
+{
+    
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting client..");
+
+            TcpClient client = new TcpClient("127.0.0.1", 5000);
+
+            NetworkStream stream = client.GetStream();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                byte[] dataToServer = Encoding.ASCII.GetBytes("Hello from client");
+                stream.Write(dataToServer, 0, dataToServer.Length);
+
+                byte[] dataFromServer = new byte[1024];
+                int bytesRead = stream.Read(dataFromServer, 0, dataFromServer.Length);
+                string respone = Encoding.ASCII.GetString(dataFromServer, 0, bytesRead);
+                Console.WriteLine(respone);
+                if (input == "exit")
+                {
+                    break;
+                }
+            }
+
+            stream.Close();
+            client.Close();
+
+        }
+    }
+}
