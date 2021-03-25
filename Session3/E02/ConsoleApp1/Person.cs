94ad497d3e6cab2faba20ec63e38a6ca55531477
littlelenka3 <56080011+littlelenka3@users.noreply.github.com>
2021-03-25 14:09:45 +0100

    Add files via upload

---
+++ Session3/E02/ConsoleApp1/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E02
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public double Height { get; set; }
+        public bool IsMarried { get; set; }
+        public char Sex { get; set; }
+        public string[] Hobbies { get; set; }
+    }
+}
---
+++ Session3/E02/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+
+namespace E02
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Lenka = new Person
+            {
+                FirstName = "Lenka",
+                LastName = "Orincakova",
+                Age = 20,
+                Height = 159,
+                IsMarried = false,
+                Sex = 'f',
+                Hobbies = new string[] { "gym, studying" }
+            };
+            var Jano = new Person
+            {
+                FirstName = "jano",
+                LastName = "lishak",
+                Age = 20,
+                Height = 179,
+                IsMarried = false,
+                Sex = 'm',
+                Hobbies = new string[] { "programming, dron" }
+            };
+
+            var Whatever = new Person
+            {
+                FirstName = "llell",
+                LastName = "lllll",
+                Age = 34,
+                Height = 390,
+                IsMarried = false,
+                Sex = 'm',
+                Hobbies = new string[] { "njn, kmik" }
+            };
+
+            var people = new List<Person> { Lenka, Jano, Whatever };
+            StoreObjects(people);
+            ReadObjects();
+        }
+        private static void StoreObjects(List<Person> people)
+        {
+            File.WriteAllText("people.Json", JsonSerializer.Serialize(people));
+
+        }
+        private static void ReadObjects()
+        {
+            var people = JsonSerializer.Deserialize<List<Person>>(File.ReadAllText("people.json"));
+        }
+    }
+}
+    
+
