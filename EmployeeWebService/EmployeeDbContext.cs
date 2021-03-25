96282d191ce681acf60954686ee69146220fec29
littlelenka3 <56080011+littlelenka3@users.noreply.github.com>
2021-03-25 13:46:09 +0100

    Add files via upload

---
+++ EmployeeWebService/EmployeeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeWebService.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeWebService
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = Employee.db");
+        }
+    }
+}
